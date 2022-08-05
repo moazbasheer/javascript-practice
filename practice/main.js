f4055994bd0bf91bf7c472a9df7c34e3ce1aef89
moazbasheer <moazbasheer97@gmail.com>
2022-08-05 12:00:40 +0200

    L10-17

--- practice/main.js
+++ practice/main.js
@@ -1,5 +1,1 @@
-window.onload = function() {
-    console.log(document.querySelector('div').childNodes.length);
-    alert(1);
-    console.log(["ahmed", "osama"]);
-}
+console.log('Elzero \'ff\'');
